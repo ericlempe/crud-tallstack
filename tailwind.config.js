01538770d9b0168415c98c765cfeccab979c9e90
Eric <dev.ericlp@gmail.com>
2024-03-02 00:38:48 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import flowbite from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        flowbite
+    ],
+}
+
